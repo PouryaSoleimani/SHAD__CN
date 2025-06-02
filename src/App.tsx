1d9d8ec113462ce38b459bb8938462dd48791b03
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-02 20:20:02 +0330

    INIT

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,20 @@
-import { Card, CardContent } from "@/components/ui/card";
-import { APITester } from "./APITester";
 import "./index.css";
-
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
+import { Button } from "./components/ui/button";
 
 export function App() {
   return (
-    <div className="container mx-auto p-8 text-center relative z-10">
-      <div className="flex justify-center items-center gap-8 mb-8">
-        <img
-          src={logo}
-          alt="Bun Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] [animation:spin_20s_linear_infinite]"
-        />
-      </div>
-
-      <Card className="bg-card/50 backdrop-blur-sm border-muted">
-        <CardContent className="pt-6">
-          <h1 className="text-5xl font-bold my-4 leading-tight">Bun + React</h1>
-          <p>
-            Edit{" "}
-            <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">src/App.tsx</code> and
-            save to test HMR
-          </p>
-          <APITester />
-        </CardContent>
-      </Card>
-    </div>
+    <section id="HOME__PAGE">
+      <h1 className="w-fit bg-white text-black px-16 py-6 rounded-xl font-black tracking-tight mx-auto mt-3">SHAD__CN TRAINING</h1>
+      <main className="flex flex-col gap-10 items-center justify-center w-screen h-screen">
+        <div className="bg-zinc-900 flex items-center justify-center gap-4 rounded-lg p-6">
+          <Button onClick={() => console.log("HELLO SHAD__CN")}>CLICK ME</Button>
+          <Button variant="default">PRIMARY</Button>
+          <Button variant="secondary">SECONDARY</Button>
+          <Button variant="destructive">DESCTRUCTIVE</Button>
+          <Button variant="blue">BLUE</Button>
+        </div>
+      </main>
+    </section>
   );
 }
 
