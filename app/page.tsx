6279f168be6ffa49343befa08f2ea74b7aebea57
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-04 22:24:22 +0330

    SHAD___CN

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { ThemeToggler } from "@/components/modules/ThemeToggler";
 import { SignUpForm } from "@/components/templates/SignUpForm";
 
 // ^ HOME PAGE ===============================================================================================================================================================
 export default function Home() {
   return (
     <section id="HOME____PAGE" className="flex flex-col items-center justify-center w-screen h-screen bg-black">
-      <div className="sticky top-0 left-0 right-0 w-full border-b-2 border-neutral-300"></div>
+      <div className="absolute top-0 left-0 right-0 w-full border-b-2 border-neutral-300 h-10">
+        <ThemeToggler />
+      </div>
       <SignUpForm />
     </section>
   );
