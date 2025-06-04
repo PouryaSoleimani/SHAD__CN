4b48791e40ac1c33c8a9d1cc9070d5ad3b0d7c52
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-04 22:19:02 +0330

    SHAD___CN

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { SignUpForm } from "@/components/templates/SignUpForm";
 export default function Home() {
   return (
     <section id="HOME____PAGE" className="flex flex-col items-center justify-center w-screen h-screen bg-black">
+      <div className="sticky top-0 left-0 right-0 w-full border-b-2 border-neutral-300"></div>
       <SignUpForm />
     </section>
   );
