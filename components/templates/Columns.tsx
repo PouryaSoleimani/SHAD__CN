9f44445dd379be84e201ebf1bf451f339a8f501b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-05 13:16:51 +0330

    SHAD___CN___TABLES

--- components/templates/Columns.tsx
+++ components/templates/Columns.tsx
@@ -2,6 +2,9 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import Image from "next/image";
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger, } from "@/components/ui/dropdown-menu"
 
 export type Payment = {
     id: string | number
@@ -47,5 +50,33 @@ export const columns: ColumnDef<Payment>[] = [
         cell: ({ row }) => {
             return <Image src={row.getValue("image")} alt="product image" width={100} height={100} className="!size-16 rounded mx-auto" />
         }
+    },
+    {
+        id: "actions",
+        header: "ACTIONS",
+        cell: ({ row }) => {
+            const payment = row.original
+
+            return (
+                <DropdownMenu>
+
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-8 w-8" />
+                        </Button>
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Product Details</DropdownMenuLabel>
+                        <DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.id.toString())}> Copy product ID</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View customer</DropdownMenuItem>
+                        <DropdownMenuItem>View payment details</DropdownMenuItem>
+                    </DropdownMenuContent>
+
+                </DropdownMenu>
+            )
+        },
     },
 ]
