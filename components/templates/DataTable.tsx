679c449245926a0cc18792d815c8bc6b82af94d3
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-05 14:23:09 +0330

    SHAD___CN___TABLES

--- components/templates/DataTable.tsx
+++ components/templates/DataTable.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable, } from "@tanstack/react-table"
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable, getPaginationRowModel, } from "@tanstack/react-table"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table"
+import { Button } from "../ui/button"
 
 
 interface DataTableProps<TData, TValue> {
@@ -10,49 +11,56 @@ interface DataTableProps<TData, TValue> {
 
 export function DataTable<TData, TValue>({ columns, data, }: DataTableProps<TData, TValue>) {
 
-    const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel(), })
+    const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel(), getPaginationRowModel: getPaginationRowModel(), initialState: { pagination: { pageSize: 5, }, }, })
 
     return (
-        <div className="rounded-md border-4 p-3 bg-neutral-900">
+        <div>
 
-            <Table>
+            <div id="TABLE___CONTAINER" className="rounded-md border-4 p-3 bg-neutral-900">
 
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id} className="w-fit nth-of-type-[1]:bg-red-600/30 nth-of-type-[2]:bg-blue-600/30 nth-of-type-[3]:bg-emerald-600/30 nth-of-type-[4]:bg-violet-600/30 nth-of-type-[5]:bg-yellow-500/30 text-white font-bold bg-neutral-950 divide-x-2 divide-white border-b-4 border-neutral-500">
-                                        {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                                    </TableHead>
-                                )
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHeader>
+                <Table>
 
-                <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow key={row.id} data-state={row.getIsSelected() && "selected"} className="nth-of-type-[odd]:bg-neutral-900 nth-of-type-[even]:bg-neutral-950 hover:!bg-black duration-500" >
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id} className="px-4 py-2">
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </TableCell>
-                                ))}
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id} className="w-fit nth-of-type-[1]:bg-red-600/30 nth-of-type-[2]:bg-blue-600/30 nth-of-type-[3]:bg-emerald-600/30 nth-of-type-[4]:bg-violet-600/30 nth-of-type-[5]:bg-yellow-500/30 text-white font-bold bg-neutral-950 divide-x-2 divide-white border-b-4 border-neutral-500">
+                                            {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                        </TableHead>
+                                    )
+                                })}
                             </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell colSpan={columns.length} className="h-24 text-center">
-                                No results.
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
+                        ))}
+                    </TableHeader>
 
-            </Table>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"} className="nth-of-type-[odd]:bg-neutral-900 nth-of-type-[even]:bg-neutral-950 hover:!bg-black duration-500" >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id} className="px-4 py-2">
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
 
+                </Table>
+
+            </div>
+            <div id="PAGINTAION___CONTAINER" className="flex items-center justify-end space-x-2 py-4">
+                <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()} >Previous</Button>
+                <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()} >Next</Button>
+            </div>
         </div>
     )
 }
