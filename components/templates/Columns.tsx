85197c0b889ed90607c6332ace0b6987ee247741
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-05 13:41:23 +0330

    SHAD___CN___TABLES

--- components/templates/Columns.tsx
+++ components/templates/Columns.tsx
@@ -2,7 +2,7 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import Image from "next/image";
-import { MoreHorizontal } from "lucide-react"
+import { Bookmark, Copy, History, MoreHorizontal, Star } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger, } from "@/components/ui/dropdown-menu"
 
@@ -61,18 +61,19 @@ export const columns: ColumnDef<Payment>[] = [
                 <DropdownMenu>
 
                     <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
+                        <Button variant="ghost" className="h-8 w-8 p-0" >
                             <span className="sr-only">Open menu</span>
                             <MoreHorizontal className="h-8 w-8" />
                         </Button>
                     </DropdownMenuTrigger>
 
                     <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Product Details</DropdownMenuLabel>
-                        <DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.id.toString())}> Copy product ID</DropdownMenuItem>
+                        <DropdownMenuLabel>Options</DropdownMenuLabel>
+                        <DropdownMenuItem onClick={() => console.info(row.original)}><Bookmark /> Save </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.id.toString())}><Copy /> Copy product ID</DropdownMenuItem>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem>View customer</DropdownMenuItem>
-                        <DropdownMenuItem>View payment details</DropdownMenuItem>
+                        <DropdownMenuItem><Star />View Ratings</DropdownMenuItem>
+                        <DropdownMenuItem><History />View Price History</DropdownMenuItem>
                     </DropdownMenuContent>
 
                 </DropdownMenu>
