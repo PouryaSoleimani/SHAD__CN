d5c0f660ecb013b9c812b13837c6e2e7888ea5b5
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-06 22:14:16 +0330

    SHAD__CN__TABLES

---
+++ app/dialog/page.tsx
@@ -0,0 +1,12 @@
+import { DialogComponent } from '@/components/modules/Dialog'
+import React from 'react'
+
+const DialogPage = () => {
+    return (
+        <div className='flex items-center justify-center h-screen'>
+            <DialogComponent />
+        </div>
+    )
+}
+
+export default DialogPage
