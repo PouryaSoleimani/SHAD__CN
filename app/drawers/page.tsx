a6d1b52631d49b5582ac5c67feaad75eecf53d5c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-06 23:22:17 +0330

    SHAD__CN__TABLES

---
+++ app/drawers/page.tsx
@@ -0,0 +1,12 @@
+import { DrawerDemo } from '@/components/modules/Drawer'
+import React from 'react'
+
+const DrawersPage = () => {
+    return (
+        <div className='flex items-center justify-center h-screen'>
+            <DrawerDemo />
+        </div>
+    )
+}
+
+export default DrawersPage
