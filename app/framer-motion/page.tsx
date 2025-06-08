07e31cee12059de040ec9a96342a812e6b07bc0d
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-08 19:49:34 +0330

    FRAMER___MOTION

--- app/framer-motion/page.tsx
+++ app/framer-motion/page.tsx
@@ -6,7 +6,7 @@ import { motion } from "motion/react"
 const FramerMotionPage = () => {
     return (
         <div className='p-10'>
-            <motion.div animate={{ rotate: 360 , x : 350 }} transition={{ duration: 1.5, ease: "easeOut", type: 'spring', damping: 200, mass : 1 }} className='size-32 bg-orange-500 rounded-xl' />
+            <motion.div animate={{ rotate: 360 , x : 350 }} transition={{ duration: 1.5, ease: "easeOut", type: 'spring', damping: 30, mass : 1 }} className='size-32 bg-orange-500 rounded-xl' />
         </div>
     )
 }
