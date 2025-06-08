fbd0fde3c99cf4bb9e2a2b483211ad1bdb8873ae
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-08 20:41:16 +0330

    FRAMER___MOTION

--- app/framer-motion/page.tsx
+++ app/framer-motion/page.tsx
@@ -1,14 +1,30 @@
-"use client"
-import React from 'react'
-import { motion } from "motion/react"
-
+"use client";
+import React from "react";
+import { motion } from "motion/react";
 
 const FramerMotionPage = () => {
     return (
-        <div className='p-10'>
-            <motion.div animate={{ rotate: 360 , x : 350 }} transition={{ duration: 1.5, ease: "easeOut", type: 'spring', damping: 30, mass : 1 }} className='size-32 bg-orange-500 rounded-xl' />
+        <div className="p-10">
+            <motion.div
+                animate={{ rotate: 360, x: 350 }}
+                transition={{
+                    duration: 1.5,
+                    ease: "easeOut",
+                    type: "spring",
+                    damping: 30,
+                    mass: 0.7,
+                }}
+                className="size-32 bg-orange-500 rounded-xl"
+            />
+            <motion.button
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
+                whileTap={{ scale: 0.8 }}
+                whileHover={{ scale: 1.1 }}
+                className="size-32 bg-pink-500 rounded-full" />
         </div>
-    )
-}
+    );
+};
 
-export default FramerMotionPage
+export default FramerMotionPage;
