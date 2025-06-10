f49d0c66b6d99a63406e7e37bc9e1b0c162409ef
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-10 22:24:08 +0330

    FRAMER___MOTION

--- app/framer-motion/page.tsx
+++ app/framer-motion/page.tsx
@@ -1,10 +1,9 @@
 // ^ FRAMER MOTION PAGE ============================================================================================================================
 "use client";
 import React from "react";
-import * as motion from "motion/react-client"
+import * as motion from "motion/react-client";
 import { AnimatePresence, useScroll } from "motion/react";
-import { useState } from "react"
-
+import { useState } from "react";
 
 const FramerMotionPage = () => {
     const { scrollYProgress } = useScroll()
@@ -20,51 +19,27 @@ const FramerMotionPage = () => {
             />
             <motion.div className="bg-white rounded-full size-24 my-4" initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0.5 }} transition={{ duration: 0.5, bounce: 0.2 }} />
 
-            <motion.button
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{ duration: 0.5, ease: "easeOut" }}
-                whileTap={{ scale: 0.8 }}
-                whileHover={{ scale: 1.1 }}
-                className="size-32 bg-pink-500 rounded-full" />
+         <motion.button initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5, ease: "easeOut" }} whileTap={{ scale: 0.8 }} whileHover={{ scale: 1.1 }} className="size-32 bg-pink-500 rounded-full" />
 
-            <button
-                id="TOGGLE___BUTTON"
-                className="toggle-container mt-6"
-                style={{ ...Container, justifyContent: "flex-" + (isOn ? "start" : "end"), backgroundColor: "#fff" }}
-                onClick={toggleSwitch}
-            >
-                <motion.div
-                    className="toggle-handle -translate-y-2.5"
-                    style={Handle}
-                    layout
-                    transition={{ type: "spring", visualDuration: 0.2, bounce: 0.2, }}
-                />
-            </button>
+         <button id="TOGGLE___BUTTON" className="toggle-container mt-6" style={{ ...Container, justifyContent: "flex-" + (isOn ? "start" : "end"), backgroundColor: "#fff" }} onClick={toggleSwitch}>
+            <motion.div className="toggle-handle -translate-y-2.5" style={Handle} layout transition={{ type: "spring", visualDuration: 0.2, bounce: 0.2 }} />
+         </button>
 
+         <AnimatePresence initial={false}>
+            <div className="flex flex-col m-10 duration-500">
+               {show ? <motion.div key="box" animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0 }} transition={{ duration: 3000 }} className="size-32 bg-red-600 rounded-md mt-10" /> : null}
+               <motion.button whileTap={{ scale: 0.95 }} className="toggle-button w-fit bg-emerald-900 p-3 rounded" onClick={() => setShow(!show)}>
+                  TOGGLE
+               </motion.button>
+            </div>
+         </AnimatePresence>
 
-            <AnimatePresence initial={false}>
-                <div className="flex flex-col m-10 duration-500">
-                    {show ? <motion.div key="box" animate={{ opacity: 1, scale: 1, }} exit={{ opacity: 0, scale: 0 }} transition={{ duration: 3000 }} className="size-32 bg-red-600 rounded-md mt-10" /> : null}
-                    <motion.button whileTap={{ scale: 0.95 }} className="toggle-button w-fit bg-emerald-900 p-3 rounded" onClick={() => setShow(!show)}>TOGGLE</motion.button>
-                </div>
-            </AnimatePresence>
-
-            {/* H-SCREEN DIV **************************************************************************************************************************/}
-            <div className="h-screen"></div>
-            <motion.div
-                id="scroll-indicator"
-                style={{ scaleX: scrollYProgress, position: "fixed", top: "60px", left: 0, right: 0, height: 20, originX: 0, backgroundColor: "#ff0088", }}
-            />
-            <motion.button
-                initial={{ scale: 0 }}
-                whileInView={{ scale: 1, x: 1100, rotate: 180 }}
-                transition={{ duration: 1, ease: "easeOut", mass: 100 }}
-                whileTap={{ scale: 0.8 }}
-                whileHover={{ scale: 1.1 }}
-                className="size-32 bg-blue-500 mx-10 rounded-md" />
-        </div>
-    );
+         {/* H-SCREEN DIV **************************************************************************************************************************/}
+         <div className="h-screen"></div>
+         <motion.div id="scroll-indicator" style={{ scaleX: scrollYProgress, position: "fixed", top: "60px", left: 0, right: 0, height: 20, originX: 0, backgroundColor: "#ff0088" }} />
+         <motion.button initial={{ scale: 0 }} whileInView={{ scale: 1, x: 1100, rotate: 180 }} transition={{ duration: 1, ease: "easeOut", mass: 100 }} whileTap={{ scale: 0.8 }} whileHover={{ scale: 1.1 }} className="size-32 bg-blue-500 mx-10 rounded-md" />
+      </div>
+   );
 };
 
 export default FramerMotionPage;
@@ -74,18 +49,18 @@ export default FramerMotionPage;
  */
 
 const Container = {
-    width: 120,
-    height: 55,
-    backgroundColor: "var(--hue-3-transparent)",
-    borderRadius: 50,
-    cursor: "pointer",
-    display: "flex",
-    padding: "12px 3px",
-}
+   width: 120,
+   height: 55,
+   backgroundColor: "var(--hue-3-transparent)",
+   borderRadius: 50,
+   cursor: "pointer",
+   display: "flex",
+   padding: "12px 3px",
+};
 
 const Handle = {
-    width: 50,
-    height: 50,
-    backgroundColor: "#9911ff",
-    borderRadius: "50%",
-}
+   width: 50,
+   height: 50,
+   backgroundColor: "#9911ff",
+   borderRadius: "50%",
+};
